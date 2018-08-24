ec923bb8400dd77d11b4e2c611076f38d1567e7a
Harry Bloch <harryfbloch@gmail.com>
2018-08-24 19:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Harry") {
+  console.log("The name is correct!")
+}else{
+  consnole.log("Name should be Harry")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
